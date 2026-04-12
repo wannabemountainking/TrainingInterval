0421bcb7d4ae46acfc59ed06ae810605e43d77c7
김도윤 <kdy0096@gmail.com>
2026-04-12 23:44:19 +0900

    make viewModel

--- TrainingInterval/IntervalViewModel.swift
+++ TrainingInterval/IntervalViewModel.swift
@@ -14,8 +14,33 @@ final class IntervalViewModel {
     var trainingMode: ExerciseMode = .trainingMode
     var trainingProgress: Int = 0
     var relaxingProgress: Int = 0
-    var timeLeft: Date =
+    
+    var timerPublisher = Timer.publish(every: 1.0, on: .main, in: .common)
+    var timeLeft: Date = Date()
+    var timerConnection: Cancellable?
     
     
+    init(trainingMode: ExerciseMode, trainingProgress: Int, relaxingProgress: Int, timerPublisher: Timer.TimerPublisher = Timer.publish(every: 1.0, on: .main, in: .common), timeLeft: Date, timerConnection: AnyCancellable? = nil) {
+        self.trainingMode = trainingMode
+        self.trainingProgress = trainingProgress
+        self.relaxingProgress = relaxingProgress
+        self.timerPublisher = timerPublisher
+        self.timeLeft = timeLeft
+        self.timerConnection = timerConnection
+    }
+    
+    func start() {
+        timerConnection = timerPublisher.connect()
+    }
+    
+    func stop() {
+        guard timerConnection != nil else { return }
+        timerConnection?.cancel()
+        timerConnection = nil
+    }
+    
+    func resume() {
+        
+    }
 }
 
